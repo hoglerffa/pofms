8e05d517f4d5cb640160221ab0ba2a20a5d14bc3
hoglerffa <93276776+hoglerffa@users.noreply.github.com>
2022-07-23 18:39:34 +0800

    Update GitHubLink.tsx

--- components/GitHubLink.tsx
+++ components/GitHubLink.tsx
@@ -2,8 +2,10 @@ import styles from "../../styles/Thirdweb.module.css";
 import React from "react";
 
 export default function ThirdwebGuideFooter() {
-  const url = "https://opensea.io/collection/pofms";
-  const url0 = "https://element.market/collections/pofms";
+  const url1 = "https://bitkeep.com/";
+  const url2 = "https://element.market/collections/pofms";
+  const url3 = "https://twitter.com/POFM_NFT";
+  
   return (
     <>
       <div
@@ -20,7 +22,7 @@ export default function ThirdwebGuideFooter() {
           cursor: "pointer",
         }}
         role="button"
-        onClick={() => window.open(url, "_blank")}
+        onClick={() => window.open(url1, "_blank")}
       />
 
       <div
@@ -28,15 +30,16 @@ export default function ThirdwebGuideFooter() {
           position: "fixed",
           bottom: 14,
           right: 18,
+          opacity: 0.7,
         }}
       >
         <img
-          src={"/opensea.png"}
+          src={"/Bitkeep.png"}
           width={40}
           height={40}
           role="button"
           style={{ cursor: "pointer" }}
-          onClick={() => window.open(url, "_blank")}
+          onClick={() => window.open(url1, "_blank")}
         />
       </div>
       
@@ -54,7 +57,7 @@ export default function ThirdwebGuideFooter() {
           cursor: "pointer",
         }}
         role="button"
-        onClick={() => window.open(url0, "_blank")}
+        onClick={() => window.open(url2, "_blank")}
       />
 
       <div
@@ -62,6 +65,7 @@ export default function ThirdwebGuideFooter() {
           position: "fixed",
           bottom: 14,
           left: 18,
+          opacity: 0.7,
         }}
       >
         <img
@@ -70,9 +74,29 @@ export default function ThirdwebGuideFooter() {
           height={40}
           role="button"
           style={{ cursor: "pointer" }}
-          onClick={() => window.open(url0, "_blank")}
+          onClick={() => window.open(url2, "_blank")}
+        />
+      </div>     
+      
+      <div
+        style={{
+          position: "fixed",
+          bottom: 500,
+          right: 8,
+          
+        }}
+      >
+        <img
+          src={"/Twitter.png"}
+          width={30}
+          height={30}
+          role="button"
+          style={{ cursor: "pointer" }}
+          onClick={() => window.open(url3, "_blank")}
         />
       </div>
+
+     
 
     </>
   );
